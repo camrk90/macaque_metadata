4f588214e75b93ad972ae887b621d89487fefa66
camrk90 <cameron.kelsey90@gmail.com>
2025-10-16 13:00:13 -0700

    edit genome annotation files

--- genome_annotation/intersect_annotations.sh
+++ genome_annotation/intersect_annotations.sh
@@ -1,19 +1,18 @@
 #!/bin/sh
 
-module load bedtools2-2.30.0-gcc-11.2.0
+#module load bedtools2-2.30.0-gcc-11.2.0
 
 ANNO=$1
-ANNO_BED=(${ANNO//./ })
+ANNO_BED="${ANNO%%.*}.bed"
 BED_FILE=$2
-FILE_NAME=(${BED_FILE//./ })
+FILE_NAME="${BED_FILE%%.*}_intersect.txt"
 
-
-#This chunk converts a .txt file to .bed file if need be and runs the intersect STILL NEEDS TO BE PROPERLY CODED
-if [[${ANNO} == *".txt"* ]]; then
-    cat ${ANNO} > ${ANNO_BED[0]}.bed
-    bedtools intersect -a ${BED_FILE} -b ${ANNO} -wo > ${FILE_NAME[0]}_intersect.txt
+#This chunk converts a .txt file to .bed file if need be and runs the intersect
+if [[ ${ANNO} == *".txt" ]]; then
+    cat ${ANNO} > ${ANNO_BED}
+    #bedtools intersect -a ${BED_FILE} -b ${ANNO} -wo > ${FILE_NAME}
 fi
 
 #intersect with the lifted over chmm file
-bedtools intersect -a ${BED_FILE} -b ${ANNO} -wo > ${FILE_NAME[0]}_intersect.txt
+#bedtools intersect -a ${BED_FILE} -b ${ANNO} -wo > ${FILE_NAME}
 
