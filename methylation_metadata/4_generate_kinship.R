9f55e5281bf250fa1263e460c2af7383db60bb6a
Cam Kelsey <ckelsey4@asu.edu>
2025-10-28 15:03:43 -0700

    update kinship script to include rna-seq metadata

--- methylation_metadata/4_generate_kinship.R
+++ methylation_metadata/4_generate_kinship.R
@@ -6,49 +6,63 @@ path<- "/scratch/ckelsey4/Cayo_meth/"
 #Import metadata
 blood_metadata<- read.table(paste(path, "blood_metadata_full.txt", sep=""))
 
-#Generate vector of animal ids
-monkey_vector<- blood_metadata$monkey_id
+rna_meta<- readRDS("/home/ckelsey4/Cayo_meth/rna_seq/Cayo_PBMC_longLPS_metadata_wELA_18Jul25.rds")
+
+rna_meta<- rna_meta %>%
+  filter(Stimulation == "H2O") %>%
+  select(c(animal_ID, sex, trapped_age, trapping_ID, Sample_ID, Seq_batch)) %>%
+  arrange(Sample_ID)
+
+#Generate vectors of animal biosample ids
+dnam_animal_ids<- blood_metadata$monkey_id
+dnam_bio_ids<- blood_metadata$lid_pid
+
+rna_animal_ids<- rna_meta$animal_ID
+rna_bio_ids<- rna_meta$Sample_ID
 
 #Import king output file
-file.king <- c("king.kin0")
+file.king <- c(paste(path, "king.kin0", sep=""))
 
-#Generate kin matrix
-kin.matrix<- kingToMatrix(file.king, estimator = "Kinship", sample.include=monkey_vector)
-kinmat<- as.matrix(kin.matrix)
+generate_kinship<- function(animal_ids, bio_ids){
+  
+  #Generate kin matrix
+  kin.matrix<- kingToMatrix(file.king, estimator = "Kinship", sample.include=animal_ids)
+  kinmat<- as.matrix(kin.matrix)
+  
+  #Arrange kinmat colnames by metadata to match r_matrix
+  kinmat<- kinmat[unique(animal_ids),unique(animal_ids)]
+  
+  #generate empty z-matrix
+  r_matrix<- data.frame(matrix(ncol = length(unique(animal_ids)), nrow=length(bio_ids)))
+  colnames(r_matrix)<- unique(animal_ids)
+  rownames(r_matrix)<- bio_ids
+  
+  #Add monkey_id column to match colnames to
+  all.equal(rownames(r_matrix), bio_ids)
+  r_matrix$ids<- animal_ids
+  
+  #Assign 1's to colnames that match ids in the id column (i.e. 1's for the same id)
+  r_matrix[sapply(colnames(r_matrix), `==`, r_matrix$ids)] <- 1
+  
+  #Remove id column and assign class matrix
+  r_matrix<- r_matrix[,-length(r_matrix)]
+  
+  r_matrix<- as.matrix(r_matrix)
+  
+  #Replace NAs with 0s
+  r_matrix[is.na(r_matrix)]<- 0
+  
+  #Multiply matrices together to get full kinship matrix
+  full_kin<- r_matrix %*% kinmat %*% t(r_matrix)
+  
+  return(full_kin)
+  
+}
 
-#Arrange kinmat colnames by metadata to match r_matrix
-kinmat<- kinmat[unique(blood_metadata$monkey_id),unique(blood_metadata$monkey_id)]
+dnam_kin<- generate_kinship(dnam_animal_ids, dnam_bio_ids)
+rna_kin<- generate_kinship(rna_animal_ids, rna_bio_ids)
 
-#Generate id vectors for Z-matrix of samples(lids) x individuals(monkey_id)
-monkey_ids<- blood_metadata %>%
-  dplyr::select(monkey_id) %>%
-  unique()
-lids<- blood_metadata %>%
-  ungroup() %>%
-  dplyr::select(lid_pid)
-
-#generate empty z-matrix
-r_matrix<- data.frame(matrix(ncol = nrow(monkey_ids), nrow=nrow(lids)))
-colnames(r_matrix)<- monkey_ids$monkey_id
-rownames(r_matrix)<- lids$lid_pid
-
-#Add monkey_id column to match colnames to
-all.equal(rownames(r_matrix), blood_metadata$lid_pid)
-r_matrix$ids<- blood_metadata$monkey_id
-
-#Assign 1's to colnames that match ids in the id column (i.e. 1's for the same id)
-r_matrix[sapply(colnames(r_matrix), `==`, r_matrix$ids)] <- 1
-
-#Remove id column and assign class matrix
-r_matrix<- r_matrix %>%
-  dplyr::select(-length(r_matrix))
-r_matrix<- as.matrix(r_matrix)
-
-#Replace NAs with 0s
-r_matrix[is.na(r_matrix)]<- 0
-
-#Multiply matrices together to get full kinship matrix
-full_kin<- r_matrix %*% kinmat %*% t(r_matrix)
 
 #Save output file as rds
-saveRDS(full_kin, paste0(path, "full_kin_matrix.rds", sep=""))
+saveRDS(dnam_kin, paste0(path, "full_kin_matrix.rds", sep=""))
+saveRDS(rna_kin, paste(path, "rna_kin_matrix.rds", sep=""))
