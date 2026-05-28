0a906d6bf0db6ea97fcaf4238cb950ad00a67a7e
Cam Kelsey <ckelsey4@asu.edu>
2026-05-28 16:08:00 -0700

    Update generate_beds script and gitignore

--- genome_annotation/generate_beds_liftover.R
+++ genome_annotation/generate_beds_liftover.R
@@ -1,62 +1,18 @@
-#THIS SCRIPT IS FOR GENERATING .BED FILES FOR GLM MODEL REGIONS AND REPEAT MASK 
-#COORDINATES FROM THE UCSC BROWSER
-#IT ALSO LIFTS OVER THE CHROMATIN STATE MAP COORDINATES FROM THE HUMAN GENOME TO
-#THE MACAQUE GENOME SO THAT ALL THESE FILES CAN BE INTERSECTED WITH BEDTOOLS
-
 library(tidyverse)
 library(GenomicFeatures)
 library(GenomicRanges)
-
-#Import genome annotation-------------------------------------------------------
-#make txdb from macaque gff
-macaque_txdb<- makeTxDbFromGFF("/scratch/ckelsey4/Cayo_meth/Macaca_mulatta.Mmul_10.110.chr.gtf")
-
-gtf_mm<- rtracklayer::import('/scratch/ckelsey4/Cayo_meth/Macaca_mulatta.Mmul_10.110.chr.gtf')
-gtf_mm_df=as.data.frame(gtf_mm)
-
-#extract unique chromosomes
-unique_chrs <- unique(seqlevels(macaque_txdb))
-
-# Select the first 22(1-20 + X/Y) unique chromosome names as a redundancy against missing chromosome levels in the txdb object
-# This will not match to the bsseq object if the chromosomes levels in txdb don't match for whatever reason
-selected_chrs <- unique_chrs[1:21]
-macaque_txdb <- keepSeqlevels(macaque_txdb, selected_chrs)
 
 ######################################
 ### Generate .bed file for regions ###
 ######################################
 #Import metadata and regions rds------------------------------------------------
-long_data<- readRDS("/scratch/ckelsey4/Cayo_meth/long_data_adjusted")
-long_data<- long_data %>%
-  arrange(lid_pid) %>%
-  filter(n > 1)
-
-regions_cov<- readRDS("/scratch/ckelsey4/Cayo_meth/regions_cov_filtered")
-
-#Filter metadata to lids in regions list
-long_data<- long_data[long_data$lid_pid %in% colnames(regions_cov[[1]]),]
-
-#Subset regions list for lids n > 1
-regions_cov<- lapply(names(regions_cov), function(x){
-  regions_cov<- subset(regions_cov[[x]], select=long_data$lid_pid)
-  return(regions_cov)
-})
-
-#Check metadata lids match the lids (cols) of a random chromosome
-all.equal(long_data$lid_pid, colnames(regions_cov[[runif(1, 1, 21)]]))
+regions_cov<- readRDS("/scratch/ckelsey4/Cayo_meth/regions_cov_filtered.rds")
 
 cov<- do.call(rbind, regions_cov)
 regions<- rownames(cov)
-regions<- str_split_i(regions, "\\.", 3)
-regions<- as.data.frame(regions)
+regions<- str_split_i(regions, "\\.", 4)
 regions<- as.data.frame(regions) %>%
   separate_wider_delim(regions, "_", names = c("chrom", "chromStart", "chromEnd"))
-
-#Arrange df by chromosome
-regions$chrom[regions$chrom == "X"]<- 21
-regions<- regions %>%
-  arrange(as.integer(chrom))
-regions$chrom[regions$chrom == 21]<- "X"
 
 #Add 'chr' string to chr col
 regions$chrom<- gsub(" ", "", paste("chr", regions$chrom, ""))
@@ -64,53 +20,40 @@ regions$chrom<- gsub(" ", "", paste("chr", regions$chrom, ""))
 #Generate .bed file
 regions_bed<- GenomicRanges::makeGRangesFromDataFrame(regions)
 
-
 ######################################
 ###   Generate .bed file for TEs   ###
 ######################################
 #Import repeatmasker repeat coordinates-----------------------------------------
-repeats<- read.table("rmsk.txt", header = F)
-repeat_header<- cbind("bin", "swScore",	"milliDiv",	"milliDel",	"milliIns",	"genoName",	"genoStart", "genoEnd", "genoLeft",
-                      "strand",	"repName", "repClass", "repFamily", "repStart",	"repEnd",	"repLeft", "id")
-colnames(repeats)<- repeat_header
+repeats<- read.table("/scratch/ckelsey4/Cayo_meth/rmsk.txt", header = F)
+repeats<- repeats[, c(6:8, 11:13)]
+colnames(repeats)<- c("chrom", "chromStart", "chromEnd", "repName", "repClass", "repFamily")
 
-repeats$genoName<- gsub("_.*", "", repeats$genoName)
-
-repeats<- repeats %>%
-  filter(!genoName == "chrUn") %>%
-  filter(!genoName == "chrY") %>%
-  filter(!genoName == 'chrM')
-
-#Export repeats as bed file for intersect
-repeats_bed<- repeats %>%
-  dplyr::select(c(genoName, genoStart, genoEnd, repName, repClass)) 
-colnames(repeats_bed)<- c("chrom", "chromStart", "chromEnd", "repName", "repClass")
+#Removes unused chrs such as "chr10_NW_021160242v1_random", "chrUn", "chrY" & "chrM"
+chrs<- unique(gsub("_.*", "", repeats$chrom))
+chrs<- chrs[!chrs %in% c("chrUn", "chrY", "chrM")]
+repeats<- repeats %>% filter(chrom %in% chrs)
 
 #Add 1 to coordinates to account for the fact that exporting to bed will subtract 1 to make it 0-based
 #the repeats file is already 0-based
-repeats_bed<- repeats_bed %>%
+repeats<- repeats %>%
   mutate(chromStart = chromStart + 1, chromEnd = chromEnd + 1)
 
-#Generate range col to more easily match joins
-repeats_bed<- repeats_bed %>%
+#Generate range col to more easily match joins in analysis script
+repeats<- repeats %>%
   mutate(range = paste(as.character(chromStart), "-", as.character(chromEnd)))
-repeats_bed$chrom<- gsub("chr", "", repeats_bed$chrom)
-repeats_bed$chrom[repeats_bed$chrom == "X"]<- 21
-repeats_bed<- repeats_bed %>%
-  arrange(as.integer(chrom))
-repeats_bed$chrom[repeats_bed$chrom == 21]<- "X"
-repeats_bed$chrom<- gsub(" ", "", paste("chr", repeats_bed$chrom, ""))
 
+#Sort chrs numerically
+repeats<- repeats[str_order(repeats$chrom, numeric = TRUE), ]
 
-######################################
-###  Liftover Macaque CHMM states  ###
-######################################
+##########################################
+### Generate .bed file for CHMM states ###
+##########################################
 #Import bed file----------------------------------------------------------------
-bed_file<- ("E062_15_coreMarks_hg38lift_dense.bed")
+bed_file<- ("/scratch/ckelsey4/Cayo_meth/E062_15_coreMarks_hg38lift_dense.bed")
 chmm_bed<- rtracklayer::import(bed_file, format = "bed")
 
 #Import ucsc chain file
-hg38_mmul10<- rtracklayer::import.chain("hg38ToRheMac10.over.chain")
+hg38_mmul10<- rtracklayer::import.chain("/scratch/ckelsey4/Cayo_meth/hg38ToRheMac10.over.chain")
 
 #Liftover hg38 coords to mmul10 assembly and sort
 seqlevelsStyle(chmm_bed) = "UCSC"
@@ -118,13 +61,11 @@ chmm_mmul<- rtracklayer::liftOver(chmm_bed, hg38_mmul10)
 chmm_mmul<- unlist(chmm_mmul)
 genome(chmm_mmul) = "mmul_10"
 chmm_mmul<- sortSeqlevels(chmm_mmul)
-selected_chrs<- gsub(" ", "", paste("chr", selected_chrs))
+selected_chrs<- paste("chr", c(1:20, "X"), sep = "")
 chmm_mmul2<- keepSeqlevels(chmm_mmul, selected_chrs, pruning.mode = "coarse")
 chmm_mmul2<- sort(chmm_mmul2)
 
-
 #Export files as .bed for intersect---------------------------------------------
-setwd('/scratch/ckelsey4/Cayo_meth/intersect_files')
-rtracklayer::export.bed(regions_bed, con = "glm_regions.bed")
-rtracklayer::export.bed(repeats_bed, con = "repeats.bed")
-rtracklayer::export.bed(chmm_mmul2, con = "chmm_mmul.bed")
+rtracklayer::export.bed(regions_bed, con = "./genome_annotation/regions.bed")
+rtracklayer::export.bed(repeats, con = "./genome_annotation/mmul_repeats.bed")
+rtracklayer::export.bed(chmm_mmul2, con = "./genome_annotation/mmul_chmm.bed")
